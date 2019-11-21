18299b4b2ed0b078d6323408d57b576a90dd3cb1
Muhammad Rusydi Al Affan <muhrusaffan@gmail.com>
2019-11-21 11:27:35 +0700

    Update cobatranslate1.py

--- cobatranslate1.py
+++ cobatranslate1.py
@@ -1,33 +1,34 @@
 # mengimport library yang diperlukan
 import RPi.GPIO as GPIO #library GPIO
 import time #library waktu
-B1 = 2 #GPIO3 yang akan disambungkan ke LED
-B2 = 3
-B3 = 4
-B4 = 17
-B5 = 27
-B6 = 22
+
+B1 = 4 #GPIO3 yang akan disambungkan ke LED
+B2 = 5
+B3 = 6
+B4 = 7
+B5 = 8
+B6 = 9
 
 def konversi(huruf):
-	if huruf == 'a' or huruf == '2':
+	if huruf == 'a' or huruf == '1':
 		return '100001'
-	elif huruf == 'b' or huruf == '3':
+	elif huruf == 'b' or huruf == '2':
 		return '110001'
-	elif huruf == 'c' or huruf == '4':
+	elif huruf == 'c' or huruf == '3':
 		return '100101'
-	elif huruf == 'd' or huruf == '5':
+	elif huruf == 'd' or huruf == '4':
 		return '100111'
-	elif huruf == 'e' or huruf == '6':
+	elif huruf == 'e' or huruf == '5':
 		return '100011'
-	elif huruf == 'f' or huruf == '7':
+	elif huruf == 'f' or huruf == '6':
 		return '110101'
-	elif huruf == 'g' or huruf == '8':
+	elif huruf == 'g' or huruf == '7':
 		return '110111'
-	elif huruf == 'h' or huruf == '9':
+	elif huruf == 'h' or huruf == '8':
 		return '110011'
-	elif huruf == 'i' or huruf == '10':
+	elif huruf == 'i' or huruf == '9':
 		return '010101'
-	elif huruf == 'j' or huruf == '1':
+	elif huruf == 'j' or huruf == '10':
 		return '010111'
 	elif huruf == 'k':
 		return '101001'
@@ -50,25 +51,25 @@ def konversi(huruf):
 	elif huruf == 't':
 		return '011111'
 	elif huruf == 'u':
-		return '101002'
+		return '101001'
 	elif huruf == 'v':
-		return '111002'
+		return '111001'
 	elif huruf == 'w':
-		return '010112'
+		return '010111'
 	elif huruf == 'x':
-		return '101102'
+		return '101101'
 	elif huruf == 'y':
-		return '101112'
+		return '101111'
 	elif huruf == 'z':
-		return '101012'
+		return '101011'
 	elif huruf == '#':
-		return '001112'
+		return '001111'
 	elif huruf == '-':
-		return '001002'
+		return '001001'
 	elif huruf == '.':
-		return '010012'
+		return '010011'
 	elif huruf == '~':
-		return '000002'
+		return '000001'
 	elif huruf == ';':
 		return '011001'
 	elif huruf == ',':
@@ -91,16 +92,18 @@ GPIO.setup(B4,GPIO.OUT)
 GPIO.setup(B5,GPIO.OUT)
 GPIO.setup(B6,GPIO.OUT)
 
-while(1):
-    input_now = input("Masukkan huruf: ")
-    print(input_now)
-    braille = konversi(input_now)
-    output(B1,int(braille[0]))
-    output(B2,int(braille[1]))
-    output(B3,int(braille[2]))
-    output(B4,int(braille[3]))
-    output(B5,int(braille[4]))
-    output(B6,int(braille[5]))
-    print(braille)
+try:
+    while(1):
+        input_now = input("Masukkan huruf: ")
+        print(input_now)
+        braille = konversi(input_now)
+        output(B1,int(braille[0]))
+        output(B2,int(braille[1]))
+        output(B3,int(braille[2]))
+        output(B4,int(braille[3]))
+        output(B5,int(braille[4]))
+        output(B6,int(braille[5]))
+        print(braille)
+except KeyboardInterrupt:
+    GPIO.cleanup() #exit
 
-GPIO.cleanup() #exit
