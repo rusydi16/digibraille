d95877583baee7f5a6c48f0a1126ab3f0d89a937
Muhammad Rusydi Al Affan <muhrusaffan@gmail.com>
2019-11-21 11:48:28 +0700

    Update cobatranslate1.py

--- cobatranslate1.py
+++ cobatranslate1.py
@@ -1,6 +1,7 @@
 # mengimport library yang diperlukan
 import RPi.GPIO as GPIO #library GPIO
 import time #library waktu
+import string
 
 B1 = 4 #GPIO3 yang akan disambungkan ke LED
 B2 = 5
@@ -9,47 +10,71 @@ B4 = 7
 B5 = 8
 B6 = 9
 
+def insert_angka(h, word):
+	if h != 0:
+		kata = word[:h]+'#'+word[h:]
+		return kata
+	else:
+		kata = '#' + word[h:]
+		return kata
+
+def insert_hurufBesar(h, word):
+	if h != 0:
+		kata = word[:h]+'~'+word[h:]
+		return kata
+	else:
+		kata = '~' + word[h:]
+		return kata
+
+def kembali_huruf(h, word):
+	kata = word[:h]+';'+word[h:]
+	return kata
+
+def huruf_besar_semua(word):
+	kata = '~~' + word
+	return kata
+
 def konversi(huruf):
 	if huruf == 'a' or huruf == '1':
-		return '100001'
+		return '100000'
 	elif huruf == 'b' or huruf == '2':
-		return '110001'
+		return '110000'
 	elif huruf == 'c' or huruf == '3':
-		return '100101'
+		return '100100'
 	elif huruf == 'd' or huruf == '4':
-		return '100111'
+		return '100110'
 	elif huruf == 'e' or huruf == '5':
-		return '100011'
+		return '100010'
 	elif huruf == 'f' or huruf == '6':
-		return '110101'
+		return '110100'
 	elif huruf == 'g' or huruf == '7':
-		return '110111'
+		return '110110'
 	elif huruf == 'h' or huruf == '8':
-		return '110011'
+		return '110010'
 	elif huruf == 'i' or huruf == '9':
-		return '010101'
-	elif huruf == 'j' or huruf == '10':
-		return '010111'
+		return '010100'
+	elif huruf == 'j' or huruf == '0':
+		return '010110'
 	elif huruf == 'k':
-		return '101001'
+		return '101000'
 	elif huruf == 'l':
-		return '100101'
+		return '100100'
 	elif huruf == 'm':
-		return '101101'
+		return '101100'
 	elif huruf == 'n':
-		return '101111'
+		return '101110'
 	elif huruf == 'o':
-		return '101011'
+		return '101010'
 	elif huruf == 'p':
-		return '111101'
+		return '111100'
 	elif huruf == 'q':
-		return '111111'
+		return '111110'
 	elif huruf == 'r':
-		return '111011'
+		return '111010'
 	elif huruf == 's':
-		return '011011'
+		return '011010'
 	elif huruf == 't':
-		return '011111'
+		return '011110'
 	elif huruf == 'u':
 		return '101001'
 	elif huruf == 'v':
@@ -71,9 +96,9 @@ def konversi(huruf):
 	elif huruf == '~':
 		return '000001'
 	elif huruf == ';':
-		return '011001'
+		return '011000'
 	elif huruf == ',':
-		return '010001'
+		return '010000'
 
 def output(pin,con):
     if con == 1:
@@ -85,7 +110,7 @@ def output(pin,con):
 
 # mengatur mode GPIO apakah model BOARD atau BCM
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(B1,GPIO.OUT) # “pin” yang dimaksud adalah GPIO3
+GPIO.setup(B1,GPIO.OUT)
 GPIO.setup(B2,GPIO.OUT)
 GPIO.setup(B3,GPIO.OUT)
 GPIO.setup(B4,GPIO.OUT)
@@ -94,16 +119,57 @@ GPIO.setup(B6,GPIO.OUT)
 
 try:
     while(1):
-        input_now = input("Masukkan huruf: ")
+        input_now = input("Masukkan kata: ")
         print(input_now)
-        braille = konversi(input_now)
-        output(B1,int(braille[0]))
-        output(B2,int(braille[1]))
-        output(B3,int(braille[2]))
-        output(B4,int(braille[3]))
-        output(B5,int(braille[4]))
-        output(B6,int(braille[5]))
-        print(braille)
+        if input_now.isupper():
+            input_now = huruf_besar_semua(input_now)
+            
+        h = 0
+        status = 0
+
+        while(h<len(input_now)):
+            if input_now[h].isdigit():
+                    if status == 1 :
+                            h=h+1
+                    else:
+                            input_now = insert_angka(h,input_now)
+                            h=h+2
+                            status = 1
+            elif input_now[h] in string.punctuation:
+                    h=h+1
+            else:
+                    if status == 1 :
+                            if input_now[h].isupper():
+                                    input_now = kembali_huruf(h,input_now)
+                                    input_now = insert_hurufBesar(h+1, input_now)
+                                    h=h+3
+                                    status = 0
+                            else:
+                                    input_now = kembali_huruf(h,input_now)
+                                    h=h+2
+                                    status = 0
+                    else:
+                            if input_now[h].isupper():
+                                    input_now = insert_hurufBesar(h, input_now)
+                                    h=h+2
+                            else:
+                                    h=h+1
+
+        input_now = input_now.lower()
+        print("Hasil Modifikasi Kata =", input_now)
+        
+        for h in range (len(input_now)):
+            braille = konversi(input_now[h])
+            output(B1,int(braille[0]))
+            output(B2,int(braille[1]))
+            output(B3,int(braille[2]))
+            output(B4,int(braille[3]))
+            output(B5,int(braille[4]))
+            output(B6,int(braille[5]))
+            print(braille)
+            time.sleep(5)
+                        
 except KeyboardInterrupt:
     GPIO.cleanup() #exit
+
 
